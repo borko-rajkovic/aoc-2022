6b885ff3bcfca044f72c495d3a31b54d84230e0b
Borko Rajkovic <rajkovicborko@gmail.com>
2022-12-06 10:21:10 +0100

    Day 06

---
+++ day_06/solution_part_1.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const path = require("path");
+const file_name = "input.txt";
+
+const input =
+  fs
+    .readFileSync(__dirname + path.sep + file_name, "utf8")
+    .match(/(.)(?!\1|.\1|..\1)(.)(?!\2|.\2)(.)(?!\3)/).index + 4;
+
+console.log(input);
+
+// More test cases:
+//
+// mjqjpqmgbljsphdztnvjfqwrcgsmlb 7
+// bvwbjplbgvbhsrlpgdmjqwftvncz 5
+// nppdvjthqldpwncqszvftbrmjlhg 6
+// nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg 10
+// zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw 11
---
+++ day_06/solution_part_2.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const path = require("path");
+const file_name = "input.txt";
+
+const input = fs.readFileSync(__dirname + path.sep + file_name, "utf8");
+
+console.log(
+  input.match(
+    /(.)(?!\1|.\1|..\1|...\1|....\1|.....\1|......\1|.......\1|........\1|.........\1|..........\1|...........\1|............\1)(.)(?!\2|.\2|..\2|...\2|....\2|.....\2|......\2|.......\2|........\2|.........\2|..........\2|...........\2)(.)(?!\3|.\3|..\3|...\3|....\3|.....\3|......\3|.......\3|........\3|.........\3|..........\3)(.)(?!\4|.\4|..\4|...\4|....\4|.....\4|......\4|.......\4|........\4|.........\4)(.)(?!\5|.\5|..\5|...\5|....\5|.....\5|......\5|.......\5|........\5)(.)(?!\6|.\6|..\6|...\6|....\6|.....\6|......\6|.......\6)(.)(?!\7|.\7|..\7|...\7|....\7|.....\7|......\7)(.)(?!\8|.\8|..\8|...\8|....\8|.....\8)(.)(?!\9|.\9|..\9|...\9|....\9)(.)(?!\10|.\10|..\10|...\10)(.)(?!\11|.\11|..\11)(.)(?!\12|.\12)(.)(?!\13)(.)/
+  ).index + 14
+);
+
+// More test cases:
+//
+// mjqjpqmgbljsphdztnvjfqwrcgsmlb: first marker after character 19
+// bvwbjplbgvbhsrlpgdmjqwftvncz: first marker after character 23
+// nppdvjthqldpwncqszvftbrmjlhg: first marker after character 23
+// nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg: first marker after character 29
+// zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw: first marker after character 26
+
+const makeRegex = (n) => {
+  const result = [];
+  for (let i = 0; i < n - 1; i++) {
+    const something = [];
+    for (let j = 0; j < n - i - 1; j++) {
+      const dots = ".".repeat(j);
+      something.push(dots + `\\${i + 1}`);
+    }
+    result.push("(.)(?!" + something.join("|") + ")");
+  }
+
+  return RegExp(result.join(""));
+};
+
+const findFirstUniqueSubstringOfNCharacters = (s, n) =>
+  s.match(makeRegex(n))?.index ? s.match(makeRegex(n)).index + n : -1;
+
+console.log(
+  findFirstUniqueSubstringOfNCharacters(
+    fs.readFileSync(__dirname + path.sep + file_name, "utf8"),
+    14
+  )
+);
